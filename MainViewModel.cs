33d756a738eb8c3d25be4f56aeb2f2a26e28a500
TAKAHATA Shinji <shinji.takahata@diva.co.jp>
2024-08-28 18:20:36 +0900

    initial commit

---
+++ MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Reactive.Bindings;
+
+namespace PreCommit_RR;
+
+/// <summary>
+/// 共通 ViewModel
+/// </summary>
+/// <remarks>
+/// シングルトン
+/// </remarks>
+class MainViewModel
+{
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // public プロパティ
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+    /// <summary>
+    /// MainViewModel のインスタンスを取得する
+    /// </summary>
+    public static MainViewModel Instance { get; } = new();
+
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // public プロパティ (Reactive)
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+    /// <summary>
+    /// 表示画像のパス
+    /// </summary>
+    public ReactivePropertySlim<string> Path_Img { get; } = new ReactivePropertySlim<string>();
+
+    /// <summary>
+    /// 表示するテキスト
+    /// </summary>
+    public ReactivePropertySlim<string> Message { get; } = new ReactivePropertySlim<string>();
+
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // コンストラクタ・ファイナライザ
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+    /// <summary>
+    /// [private] コンストラクタ
+    /// </summary>
+    private MainViewModel() 
+    {
+    }
+
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // public メソッド
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+}
---
+++ WaitingScreen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using PreCommit_RR;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Util;
+/// <summary>
+/// WaitingScreen.xaml の相互作用ロジック
+/// </summary>
+public partial class WaitingScreen : Window
+{
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // private フィールド
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+    /// <summary>
+    /// 現在表示中のファイル番号
+    /// </summary>
+    private int fileNum = 0;
+
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // コンストラクタ・ファイナライザ
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public WaitingScreen( string message = "処理中です…" )
+    {
+        InitializeComponent();
+
+        DataContext = MainViewModel.Instance;
+
+        MainViewModel.Instance.Message.Value = message;
+        ReNewImgPath();
+
+        // 表示更新用タイマの設定、起動
+        DispatcherTimer timer = new DispatcherTimer();
+        timer.Interval = new TimeSpan( 500000 );
+        timer.Tick += ( s, e ) => ReNewImgPath();
+
+        Loaded += (s, e) => timer.Start();
+    }
+
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+    // private メソッド
+    // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+    /// <summary>
+    /// 画像の更新
+    /// </summary>
+    /// <remarks>タイマ発火時にコールされることを想定</remarks>
+    private void ReNewImgPath()
+    {
+        fileNum++;
+        if( fileNum > 24 )
+        {
+            fileNum = 1;
+        }
+
+        ( (MainViewModel)DataContext ).Path_Img.Value = $@"_img\{fileNum:000}.png";
+    }
+
+}
